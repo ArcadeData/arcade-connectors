3d0cff89e262626b91235d98df0812b8df14d01b
robfrank <ro.franchini@gmail.com>
2019-02-19 15:10:31 +0100

    bumps orientdb image used in tests to 2.2.37

--- neo4j3/src/test/kotlin/com/arcadeanalytics/provider/neo4j3/Neo4jTestHelper.kt
+++ neo4j3/src/test/kotlin/com/arcadeanalytics/provider/neo4j3/Neo4jTestHelper.kt
@@ -28,7 +28,7 @@ import org.testcontainers.containers.wait.strategy.Wait
 
 object Neo4jContainer {
 
-    val container: KGenericContainer = KGenericContainer("neo4j:latest")
+    private val container: KGenericContainer = KGenericContainer("neo4j:latest")
             .apply {
                 withExposedPorts(7687, 7474)
                 withEnv("NEO4J_AUTH", "neo4j/arcade")
