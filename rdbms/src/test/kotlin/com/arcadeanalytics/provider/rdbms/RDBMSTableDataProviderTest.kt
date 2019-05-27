d965778bf309512348006f56f971f107df0e5134
robfrank <ro.franchini@gmail.com>
2019-05-27 16:08:44 +0200

    #54
    improves test for RDBMSTableDataProvider

--- rdbms/src/test/kotlin/com/arcadeanalytics/provider/rdbms/RDBMSTableDataProviderTest.kt
+++ rdbms/src/test/kotlin/com/arcadeanalytics/provider/rdbms/RDBMSTableDataProviderTest.kt
@@ -1,7 +1,10 @@
 package com.arcadeanalytics.provider.rdbms
 
 import com.arcadeanalytics.provider.DataSourceInfo
+import com.arcadeanalytics.provider.TABLE_CLASS
+import com.arcadeanalytics.provider.TypeProperties
 import com.arcadeanalytics.provider.rdbms.dataprovider.PostgreSQLContainerHolder
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.PostgreSQLContainer
@@ -44,7 +47,28 @@ internal class RDBMSTableDataProviderTest {
             | ORDER BY total_amount DESC
             | """.trimMargin(), 100)
 
-        data.nodes.forEach { n -> println("n = ${n}") }
+        val tableClass = data.nodesClasses[TABLE_CLASS]
+
+        Assertions.assertThat(tableClass).containsKeys("name", "cardinality", "properties")
+
+        val properties: TypeProperties = tableClass?.get("properties") as TypeProperties
+
+        Assertions.assertThat(properties).containsKeys("customer_id", "total_amount")
+
+
+        val cytoData = data.nodes.first()
+        Assertions.assertThat(cytoData.classes).isEqualTo(TABLE_CLASS)
+
+        Assertions.assertThat(cytoData.group)
+                .isEqualTo("nodes")
+
+        Assertions.assertThat(cytoData.data.id).isEqualTo("0")
+        Assertions.assertThat(cytoData.data.source).isEmpty()
+        Assertions.assertThat(cytoData.data.target).isEmpty()
+
+        val record = cytoData.data.record
+
+        Assertions.assertThat(record).containsOnlyKeys("customer_id", "total_amount")
 
     }
 }
