349a3425a89fba88b2341ea70d23894e04efc8fe
robfrank <ro.franchini@gmail.com>
2019-06-03 15:16:48 +0200

    refactors: improves readability

--- rdbms/src/main/kotlin/com/arcadeanalytics/provider/rdbms/RDBMSTableDataProvider.kt
+++ rdbms/src/main/kotlin/com/arcadeanalytics/provider/rdbms/RDBMSTableDataProvider.kt
@@ -3,6 +3,7 @@ package com.arcadeanalytics.provider.rdbms
 import com.arcadeanalytics.provider.*
 import com.arcadeanalytics.provider.rdbms.persistence.util.DBSourceConnection
 import java.sql.ResultSet
+import java.sql.ResultSetMetaData
 
 
 class RDBMSTableDataProvider : DataSourceTableDataProvider {
@@ -17,50 +18,58 @@ class RDBMSTableDataProvider : DataSourceTableDataProvider {
 
                     val md = rs.metaData
                     val columns = md.columnCount
-                    val cytoNodes = mutableSetOf<CytoData>()
-                    var count: Long = 0
-                    while (rs.next()) {
-                        val row = mutableMapOf<String, Any>()
-                        for (i in 1..columns) {
-                            row[md.getColumnName(i)] = rs.getObject(i)
-                        }
 
-                        val data = Data(id = count++.toString(), record = row)
+                    val cytoNodes = mapRows(rs, columns, md)
 
-                        val cytoData = CytoData(classes = TABLE_CLASS,
-                                group = "nodes",
-                                data = data)
-
-                        cytoNodes.add(cytoData)
-                    }
-
-                    val nodesProperties = mutableMapOf<String, TypeProperty>()
-                    for (i in 1..columns) {
-
-                        val columnName = md.getColumnName(i)
-
-                        nodesProperties[columnName] = TypeProperty(name = columnName, type = mapType(md.getColumnTypeName(i)))
-                    }
+                    val nodesProperties = mapMetadata(columns, md)
 
                     val tableClass = mutableMapOf<String, Any>()
                     tableClass["name"] = TABLE_CLASS
-                    tableClass["cardinality"] = count
+                    tableClass["cardinality"] = cytoNodes.size
                     tableClass["properties"] = nodesProperties
                     val nodeClasses = mutableMapOf<String, MutableMap<String, Any>>()
                     nodeClasses[TABLE_CLASS] = tableClass;
 
-
                     val edgeClasses = mutableMapOf<String, MutableMap<String, Any>>()
                     val cytoEdges = emptySet<CytoData>()
 
-
                     return GraphData(nodeClasses, edgeClasses, cytoNodes, cytoEdges)
-
                 }
             }
         }
+    }
 
-        return GraphData.EMPTY
+    private fun mapMetadata(columns: Int, md: ResultSetMetaData): MutableMap<String, TypeProperty> {
+        val nodesProperties = mutableMapOf<String, TypeProperty>()
+
+        for (i in 1..columns) {
+
+            val columnName = md.getColumnName(i)
+            val columnTypeName = md.getColumnTypeName(i)
+
+            nodesProperties[columnName] = TypeProperty(name = columnName, type = mapType(columnTypeName))
+        }
+        return nodesProperties
+    }
+
+    private fun mapRows(rs: ResultSet, columns: Int, md: ResultSetMetaData): MutableSet<CytoData> {
+        val cytoNodes = mutableSetOf<CytoData>()
+        var cardinality: Long = 0
+        while (rs.next()) {
+            val record = mutableMapOf<String, Any>()
+            for (i in 1..columns) {
+                record[md.getColumnName(i)] = rs.getObject(i)
+            }
+
+            val data = Data(id = cardinality++.toString(), record = record)
+
+            val cytoData = CytoData(classes = TABLE_CLASS,
+                    group = "nodes",
+                    data = data)
+
+            cytoNodes.add(cytoData)
+        }
+        return cytoNodes
     }
 
 
